225b37155557092f91105e3c94669983ee33b187
jj12345123 <junno.jani@hotmail.com>
2020-09-13 15:59:46 +0930

    Added service worker file with a code to new branch (service-woker-branch)

---
+++ sw.js
@@ -0,0 +1,33 @@
+var CACHE_TITLE = 'my-site-cache';
+var CACHE_VERSION = 'v1';
+var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION;
+var urlsToCache = [
+  '/HIT238_App/',
+  '/HIT238_App/styles.css',
+  '/HIT238_App/scripts.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Adding offline functionality
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
